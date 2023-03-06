f30dc465c3f1257048a975ff70bbf3c81ceb4041
NikSankovich <nik.sankovich@gmail.com>
2023-03-05 21:50:24 -0600

    first

--- main.js
+++ main.js
@@ -1,34 +1,70 @@
+// fetch a user and display them one at a time.
 window.onload = function () {
-    getContacts()
+    addressBook()
+}
+
+let people = [];
+const container = document.getElementById('container')
+
+const API = 'https://randomuser.me/api/?results=5'
+
+const addressBook = () => {
+    fetch(API)
+        .then(res => {
+            if (!res.ok) {
+                throw Error(res.statusText)
+            } return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            people = data.results
+        })
+        .catch(err => console.log(`Error, ${err}`))
+        .then(() => {
+            people.forEach((person) => {
+                let content = document.createElement("div")
+                content.innerHTML = `
+            <h4>${person.name.first} ${person.name.last}</h4>
+            <img src="${person.picture.large}"/>
+            <button id="${person.login.uuid}" onclick="showHide(this)">More Info/Less Info</button>
+            <div class="moreInfo" id="${person.login.uuid}-person">
+            Email: ${person.email} <br>
+            Phone: ${person.phone} <br>
+            Address: ${person.location.street.number} ${person.location.street.name}, 
+            ${person.location.city}, ${person.location.state} ${person.location.postcode}
+            </div>
+            `
+
+                container.appendChild(content)
+            })
+        })
+}
+
+// `what to put in the container
+// `arrayOfContacts[0]
+//  .name.first, .name.last, 
+//  .picture,medium
+//  button to show more on click
+// `persons's address, phone, email.
+
+
+// => { // build your address card with dot notation each.name.first}
+
+// }
+
+
+const showHide = (button) => {
+    console.log(button)
+    let x = document.getElementById(button.id + "-person")
+    console.log(x, 'this is the el clicked')
+    console.log(x.style.display, 'this is the current style')
+    if (x.style.display === "none" || !x.style.display) {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
 }
 
 
-// fetch a user and display them one at a time
-//let arrayOfContacts = [];
 
-let addressList = document.getElementById("container");
-
-const getContacts = () => {
-    fetch('https://randomuser.me/api/?results=5')
-        .then(res => res.json())
-        .then(data => people = data.results)
-        .then(people => {
-            people.map((person, i) => {
-                let html = `
-            <h4>${person.name.first}  ${person.name.last}</h4>
-            <img src="${person.picture.large}">
-
-
-
-            `; // end of html interpolated string
-                addressList.insertAdjacentHTML("afterbegin", html);
-            }) // } end callback in map, ) end map function 
-        }) // } end people function, ) end .then definition 
-} // end getContacts
-
-// need show/hide function 
-//div with otherinfo
-
-
-
-//}
+// const assert = require ('assert')
